96d0bbc18820bd491f24d0c66ab81f8273757425
Scott Hoisington <hoyzington@gmail.com>
2021-04-23 20:07:42 -0400

    setup redux, store, & thunk; stub out reducer; add folders to src

--- src/index.js
+++ src/index.js
@@ -1,15 +1,21 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
-// import { createStore, applyMiddleware } from 'redux'
-// import thunk from 'redux-thunk'
-// import { Provider } from 'react-redux'
+import { createStore, applyMiddleware, compose } from 'redux'
+import reducer from './reducers/reducer'
+import thunk from 'redux-thunk'
+import { Provider } from 'react-redux'
 import './index.css'
 import App from './App'
 // import reportWebVitals from './reportWebVitals'
 
+const composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose
+const store = createStore(reducer, composeEnhancers(applyMiddleware(thunk)))
+
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    <Provider store={store}>
+      <App />
+    </Provider>
   </React.StrictMode>,
   document.getElementById('root')
 )
---
+++ src/reducers/reducer.js
@@ -0,0 +1,11 @@
+const reducer = (state={items: []}, action) => {
+  // switch (action.type) {
+  //   case 'ADD':
+  //     return state
+  
+  //   default:
+      return state
+  // }
+}
+
+export default reducer
