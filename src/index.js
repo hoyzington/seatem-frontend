d20261bc24011522d90880f0310b6e614ffdfc0a
Scott Hoisington <hoyzington@gmail.com>
2021-05-30 20:14:05 -0400

    prepare to use multiple reducers

--- src/index.js
+++ src/index.js
@@ -1,6 +1,6 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
-import { createStore, applyMiddleware, compose } from 'redux'
+import { createStore, applyMiddleware, compose, combineReducers } from 'redux'
 import eventsReducer from './reducers/eventsReducer'
 import thunk from 'redux-thunk'
 import { Provider } from 'react-redux'
@@ -8,8 +8,11 @@ import { Provider } from 'react-redux'
 import App from './App'
 // import reportWebVitals from './reportWebVitals'
 
+const reducers = combineReducers({
+  eventsReducer,
+})
 const composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose
-const store = createStore(eventsReducer, composeEnhancers(applyMiddleware(thunk)))
+const store = createStore(reducers, composeEnhancers(applyMiddleware(thunk)))
 
 ReactDOM.render(
   <React.StrictMode>
