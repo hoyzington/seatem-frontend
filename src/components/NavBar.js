d70d0c6b327a9edf00edc5b3f485bfd4c7c019ea
Scott Hoisington <hoyzington@gmail.com>
2021-06-04 19:21:31 -0400

    change NavBar to class component; change Account link to Log Out; make rendering dependent on login

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -1,23 +1,57 @@
 import React from 'react'
 import { NavLink } from 'react-router-dom'
+import { connect } from 'react-redux'
+import { logout } from '../actions/sessions'
 
-const NavBar = ({ event }) => (
-  <header>
-    <nav>
-      <span id='logo-bar'>
-        <span id='logo'><i>Seat 'em</i></span>
-        <NavLink className='navbar' to='/about'>About</NavLink>
-      </span>
-      <span id='title-bar'>
-        <span><i>{event ? event.name : ''}</i></span>
-      </span>
-      <span id='menu-bar'>
-        <NavLink className='navbar' to='/event-form'>Event</NavLink>
-        <NavLink className='navbar' to='/events'>Events</NavLink>
-        <NavLink className='navbar' to='/account'>Account</NavLink>
-      </span>
-    </nav>
-  </header>
-)
+class NavBar extends React.Component {
 
-export default NavBar
+  renderLeftLinks = () => {
+    if (this.props.user && this.props.user.username) {
+      return (
+        <>
+          <NavLink className='navbar' to='/' onClick={this.props.logout}>Log Out</NavLink>
+          <NavLink className='navbar' to='/about'>About</NavLink>
+        </>
+      )
+    }
+  }
+
+  renderRightLinks = () => {
+    if (this.props.user && this.props.user.username) {
+      return (
+        <>
+          <NavLink className='navbar' to='/edit-event'>This Event</NavLink>
+          <NavLink className='navbar' to='/events'>My Events</NavLink>
+          <NavLink className='navbar' to='/new-event'>New Event</NavLink>
+        </>
+      )
+    }
+  }
+
+  render() {
+    const { event } = this.props
+    return (
+      <header>
+        <nav>
+          <span id='logo-bar'>
+            <span id='logo'><i>Seat 'em</i></span>
+            {this.renderLeftLinks()}
+          </span>
+          <span id='title-bar'>
+            <i>{event ? event.name : ''}</i>
+          </span>
+          <span id='menu-bar'>
+            {this.renderRightLinks()}
+          </span>
+        </nav>
+      </header>
+    )
+  }
+}
+
+const mapStateToProps = (state) => ({
+  user: state.currentUser,
+  events: state.events.savedEvents,
+})
+
+export default connect(mapStateToProps, { logout })(NavBar)
