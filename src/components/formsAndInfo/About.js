38f604505b209cf6199a60cd6b97d3a4f4b1dcc8
Scott Hoisington <hoyzington@gmail.com>
2021-06-04 19:11:01 -0400

    completely rebuild About component

--- src/components/formsAndInfo/About.js
+++ src/components/formsAndInfo/About.js
@@ -1,10 +1,35 @@
 import React from 'react'
-import AboutOrFormArea from './AboutOrFormArea'
+import { NavLink } from 'react-router-dom'
 
-const content = <div><p>Lorem ipsum, dolor sit amet consectetur adipisicing elit. In natus ullam maiores labore provident corporis voluptatum ex perspiciatis saepe.</p><p>Iusto quis inventore eveniet cumque, distinctio incidunt corporis doloremque magnam nostrum. Vel voluptatibus culpa odio ab nobis quam voluptate nulla atque quibusdam, provident eaque quae id, dolorum iure iusto eius aut cumque nisi.</p><p>Minus quod ratione provident porro amet accusantium cumque iure molestiae, cum maxime veniam error. Quae nihil ducimus esse debitis non, culpa molestias!</p></div>
+const renderExit = (user) => {
+  if (user) {
+    return (<NavLink id='exit' to='/'>&times;</NavLink>)
+  }
+}
 
-const About = () => (
-  <AboutOrFormArea id='about' class='card' content={content} />
-)
+const renderButton = (user) => {
+  if (user) {
+    return (<NavLink className='btn form bottom' to='/new-event'>CLICK HERE TO BEGIN</NavLink>)
+  }
+  return (<NavLink className='btn form bottom' to='/login-signup'>LOG IN or SIGN UP</NavLink>)
+}
+
+const About = (props) => {
+
+  return (
+    <div id='about' className='card'>
+      {renderExit(props.user)}
+      <p>Welcome to Seat'em, your go-to event seating arrangement helper!
+      </p>
+      <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit. In natus ullam maiores labore provident corporis voluptatum ex perspiciatis saepe.
+      </p>
+      <p>Iusto quis inventore eveniet cumque, distinctio incidunt corporis doloremque magnam nostrum. Vel voluptatibus culpa odio ab nobis quam voluptate nulla atque quibusdam, provident eaque quae id, dolorum iure iusto eius aut cumque nisi.
+      </p>
+      <div id='btn-area'>
+        {renderButton(props.user)}
+      </div>
+    </div>
+  )
+}
 
 export default About
