28ccbc7d9c279d5628ac9da4770c0712098ff553
Scott Hoisington <hoyzington@gmail.com>
2021-06-04 19:35:50 -0400

    add EventMenu component

---
+++ src/components/formsAndInfo/EventMenu.js
@@ -0,0 +1,25 @@
+import React from 'react'
+import { NavLink } from 'react-router-dom'
+
+const EventMenu = (props) => {
+  const possibleLinks = () => {
+    if (props.event.guests.length > 0) {
+      return (
+        <>
+          <div><NavLink className='event' to='/add-preferences'>Add Preferences</NavLink></div>
+          <div><NavLink className='event' to='/checklist'>Edit Guest Traits</NavLink></div>
+        </>
+      )
+    }
+  }
+  return (
+    <div id='event-menu' className='card'>
+      <NavLink id='exit' to='/'>&times;</NavLink>
+      <div><NavLink className='event' to='/update-event'>Edit Event</NavLink></div>
+      <div><NavLink className='event' to='/add-guests'>Add Guests</NavLink></div>
+      {possibleLinks()}
+    </div>
+  )
+}
+
+export default EventMenu
