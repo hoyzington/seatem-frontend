8bf588d4becc4fb215264a86ed0ad0d01a630da4
Scott Hoisington <hoyzington@gmail.com>
2021-06-10 15:14:38 -0400

    add errors reducer

--- src/store.js
+++ src/store.js
@@ -1,11 +1,13 @@
 import { createStore, applyMiddleware, compose, combineReducers } from 'redux'
-import events from './reducers/events'
 import currentUser from './reducers/currentUser'
+import events from './reducers/events'
+import errors from './reducers/errors'
 import thunk from 'redux-thunk'
 
 const reducers = combineReducers({
-  events,
   currentUser,
+  events,
+  errors,
 })
 
 const composeEnhancer = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose
