16f98269e553d36af2406704086015f51cf00210
Scott Hoisington <hoyzington@gmail.com>
2021-06-12 18:07:37 -0400

    add deleteUser to component

--- src/components/formsAndInfo/MyProfile.js
+++ src/components/formsAndInfo/MyProfile.js
@@ -1,7 +1,7 @@
 import React from 'react'
 import { NavLink } from 'react-router-dom'
 
-const MyProfile = ({ user }) => (
+const MyProfile = ({ user, deleteUser }) => (
   <div id='my-profile' className='card'>
     <NavLink id='exit' to='/' >&times;</NavLink>
     <p><b>MY PROFILE</b></p>
@@ -10,6 +10,7 @@ const MyProfile = ({ user }) => (
     <div id='my-profile-italic'>
       <i>Click Edit to change profile and/or password</i>
     </div>
+    <NavLink id='delete-account' to='/' onClick={() => deleteUser(user.id)} ><b>Delete Account</b></NavLink>
     <div id='btn-area'>
       <NavLink className='btn form bottom' to='/edit-profile'>EDIT</NavLink>
     </div>
