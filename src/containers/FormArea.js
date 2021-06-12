99015869b8d12e2131bb8b447b077ca88e3b0995
Scott Hoisington <hoyzington@gmail.com>
2021-06-12 17:10:30 -0400

    add route for EditProfile; add props to Account; move getCurrentUser into component; debug event card showing on form error

--- src/containers/FormArea.js
+++ src/containers/FormArea.js
@@ -1,9 +1,12 @@
 import React from 'react'
 import { Route, Redirect } from 'react-router-dom'
+import { connect } from 'react-redux'
+import { getCurrentUser, logout } from '../actions/currentUser'
 import About from '../components/formsAndInfo/About'
 import LoginSignupForm from '../components/formsAndInfo/LoginSignupForm'
 import Account from '../components/formsAndInfo/Account'
 import MyProfile from '../components/formsAndInfo/MyProfile'
+import EditProfile from '../components/formsAndInfo/EditProfile'
 import EventsList from '../components/formsAndInfo/EventsList'
 import NewEventForm from '../components/formsAndInfo/NewEventForm'
 import GuestForm from '../components/formsAndInfo/GuestForm'
@@ -11,33 +14,74 @@ import PreferencesForm from '../components/formsAndInfo/PreferencesForm'
 import ChecklistForm from '../components/formsAndInfo/ChecklistForm'
 import EventMenu from '../components/formsAndInfo/EventMenu'
 
-const renderFormOrInfo = (user, event) => {
-  if (user) {
-    if (user.error || user.errors) {
-      return (<Redirect to='/login-signup' />)
-    } else if (!event) {
-      return (<Redirect to='/events' />)
+class FormArea extends React.Component {
+
+  state = { startCard: true }
+
+  componentDidMount() {
+    if (!this.props.user) {
+      this.props.getCurrentUser()
     }
-  } else {
-    return (<Redirect to='/about' />)
+  }
+
+  renderFormOrInfo = (showStartCard) => {
+    const { user, event, errors } = this.props
+    if (errors) {
+      if (this.state.startCard) {
+        this.setState({ startCard: false })
+      }
+      switch (errors.type) {
+        case 'login':
+        case 'signup':
+          return (<Redirect to='/login-signup' />)
+        case 'userUpdate':
+          return (<Redirect to='/edit-profile' />)
+        case 'newEvent':
+          return (<Redirect to='/new-event' />)
+        // case 'eventUpdate':
+        //   return (<Redirect to='/edit-event' />)
+        case 'newGuest':
+          return (<Redirect to='/add-guests' />)
+        case 'guestUpdate':
+          return (<Redirect to='/add-preferences' />)
+        default:
+          break;
+      }
+    } else if (!event && this.state.startCard) {
+      if (user) {
+        return (<Redirect to='/events' />)
+      } else {
+        return (<Redirect to='/about' />)
+      }
+    }
+  }
+
+  render() {
+    const { user, event } = this.props
+    return (
+      <div id='form-area'>
+        <Route exact path='/about' render={() => (<About user={user} />)} />
+        <Route exact path='/login-signup' component={LoginSignupForm} />
+        <Route exact path='/account' render={() => (<Account logout={logout} />)} />
+        <Route exact path='/my-profile' render={() => (<MyProfile user={user} />)} />
+        <Route exact path='/edit-profile' component={EditProfile} />
+        <Route exact path='/new-event' component={NewEventForm} />
+        {/* <Route exact path='/edit-event' component={EventForm} /> */}
+        <Route exact path='/add-guests' component={GuestForm} />
+        <Route exact path='/add-preferences' component={PreferencesForm} />
+        <Route exact path='/checklist' component={ChecklistForm} />
+        <Route exact path='/events' component={EventsList} />
+        <Route exact path='/event-menu' render={() => (<EventMenu event={event} />)} />
+        {this.renderFormOrInfo()}
+      </div>
+    )
   }
 }
 
-const FormArea = (props) => (
-  <div id='form-area'>
-    {renderFormOrInfo(props.user, props.event)}
-    <Route exact path='/about' render={() => (<About user={props.user} />)} />
-    <Route exact path='/login-signup' component={LoginSignupForm} />
-    <Route exact path='/account' component={Account} />
-    <Route exact path='/my-profile' component={MyProfile} />
-    <Route exact path='/new-event' component={NewEventForm} />
-    {/* <Route exact path='/edit-event' component={EventForm} /> */}
-    <Route exact path='/add-guests' component={GuestForm} />
-    <Route exact path='/add-preferences' component={PreferencesForm} />
-    <Route exact path='/checklist' component={ChecklistForm} />
-    <Route exact path='/events' component={EventsList} />
-    <Route exact path='/event-menu' render={() => (<EventMenu event={props.event} />)} />
-  </div>
-)
+const mapStateToProps = (state) => ({
+  user: state.currentUser,
+  event: state.events.currentEvent,
+  errors: state.errors,
+})
 
-export default FormArea
+export default connect(mapStateToProps, { getCurrentUser })(FormArea)
