d08ea239f228e57ded40ec0001ae5dc7f9bf4c46
Scott Hoisington <hoyzington@gmail.com>
2021-06-19 00:15:00 -0400

    comment out possibly unneeded code; rename DELETE_GUEST, DESTROY_GUEST

--- src/reducers/events.js
+++ src/reducers/events.js
@@ -1,155 +1,157 @@
 // import { v4 as uuidv4 } from 'uuid'
 
-const updatePreviousNeighbor = (guestBefore, neighborId, guests) => {
-  const guest = guests.find(guest => guest.id === neighborId)
-  const updatedNeighbors = guest.neighbors.filter(nbrId => nbrId !== guestBefore.id)
-  return {
-    ...guest,
-    neighbors: updatedNeighbors,
-  }
-}
+// const updatePreviousNeighbor = (guestBefore, neighborId, guests) => {
+//   const guest = guests.find(guest => guest.id === neighborId)
+//   const updatedNeighbors = guest.neighbors.filter(nbrId => nbrId !== guestBefore.id)
+//   return {
+//     ...guest,
+//     neighbors: updatedNeighbors,
+//   }
+// }
 
-const updatePreviousNeighbors = (guestBefore, guests) => {
-  const prevNeighborIds = guestBefore.neighbors.filter(guestId => guestId !== '')
-  if (prevNeighborIds.length > 0) {
-    return prevNeighborIds.map(id => updatePreviousNeighbor(guestBefore, id, guests))
-  }
-  return prevNeighborIds
-}
+// const updatePreviousNeighbors = (guestBefore, guests) => {
+//   const prevNeighborIds = guestBefore.neighbors.filter(guestId => guestId !== '')
+//   if (prevNeighborIds.length > 0) {
+//     return prevNeighborIds.map(id => updatePreviousNeighbor(guestBefore, id, guests))
+//   }
+//   return prevNeighborIds
+// }
 
-const getNewNeighborIds = (chairs, guest) => {
-  const chairIdx = chairs.findIndex(chair => chair === guest.id)
-  let neighbors
-  const last = chairs.length - 1
-  switch (chairIdx) {
-    case 0:
-      neighbors = [
-        chairs[last],
-        chairs[1],
-      ]
-      break
-    case last:
-      neighbors = [
-        chairs[last - 1],
-        chairs[0],
-      ]
-      break
-    default:
-      neighbors = [
-        chairs[chairIdx - 1],
-        chairs[chairIdx + 1],
-      ]
-  }
-  return neighbors.filter(nbrId => nbrId !== '')
-}
+// const getNewNeighborIds = (chairs, guest) => {
+//   const chairIdx = chairs.findIndex(chair => chair === guest.id)
+//   let neighbors
+//   const last = chairs.length - 1
+//   switch (chairIdx) {
+//     case 0:
+//       neighbors = [
+//         chairs[last],
+//         chairs[1],
+//       ]
+//       break
+//     case last:
+//       neighbors = [
+//         chairs[last - 1],
+//         chairs[0],
+//       ]
+//       break
+//     default:
+//       neighbors = [
+//         chairs[chairIdx - 1],
+//         chairs[chairIdx + 1],
+//       ]
+//   }
+//   return neighbors.filter(nbrId => nbrId !== '')
+// }
 
-const updateNewNeighbor = (guests, neighborId, chairs) => {
-  const newNeighbor = guests.find(guest => guest.id === neighborId)
-  return {
-    ...newNeighbor,
-    neighbors: getNewNeighborIds(chairs, newNeighbor)
-  }
-}
+// const updateNewNeighbor = (guests, neighborId, chairs) => {
+//   const newNeighbor = guests.find(guest => guest.id === neighborId)
+//   return {
+//     ...newNeighbor,
+//     neighbors: getNewNeighborIds(chairs, newNeighbor)
+//   }
+// }
 
-const updateNewNeighbors = (guests, neighborIds, chairs) => {
-  if (neighborIds.length > 0) {
-    return neighborIds.map(id => updateNewNeighbor(guests, id, chairs))
-  } else {
-    return []
-  }
-}
+// const updateNewNeighbors = (guests, neighborIds, chairs) => {
+//   if (neighborIds.length > 0) {
+//     return neighborIds.map(id => updateNewNeighbor(guests, id, chairs))
+//   } else {
+//     return []
+//   }
+// }
 
-const makeInitials = (guest) => {
-  return [
-    guest.firstName[0],
-    guest.middleName[0],
-    guest.lastName[0],
-  ].join('')
-}
+// --------------------------------------------
 
-const checkForIssues = (thisGuest, guests) => {
-  const { guestsYes, guestsNo, descriptionsYes, descriptionsNo } = thisGuest
-  const neighbors = thisGuest.neighbors.map(nbrId => guests.find(guest => guest.id === nbrId))
-  const thisGuestInitials = makeInitials(thisGuest)
+// const makeInitials = (guest) => {
+//   return [
+//     guest.firstName[0],
+//     guest.middleName[0],
+//     guest.lastName[0],
+//   ].join('')
+// }
 
-  const missingNeighbors = () => {
-    if (guestsYes.length > 0) {
-      let issues = []
-      guestsYes.forEach((guestId) => {
-        if (neighbors.length === 0 || !(neighbors.find(nbr => nbr.id === guestId))) {
-          const missingNeighbor = guests.find(guest => guest.id === guestId)
-          issues.push(`${makeInitials(missingNeighbor)} should sit next to ${thisGuestInitials}`)
-        }
-      })
-      return issues
-    } else {
-      return []
-    }
-  }
+// const checkForIssues = (thisGuest, guests) => {
+//   const { guestsYes, guestsNo, descriptionsYes, descriptionsNo } = thisGuest
+//   const neighbors = thisGuest.neighbors.map(nbrId => guests.find(guest => guest.id === nbrId))
+//   const thisGuestInitials = makeInitials(thisGuest)
 
-  const wrongNeighbors = () => {
-    if (guestsNo.length > 0 && neighbors.length > 0) {
-      let issues = []
-      guestsNo.forEach((guestId) => {
-        const wrongNeighbor = neighbors.find(nbr => nbr.id === guestId)
-        if (wrongNeighbor) {
-          issues.push(`${makeInitials(wrongNeighbor)} should not sit next to ${thisGuestInitials}`)
-        }
-      })
-      return issues
-    } else {
-      return []
-    }
-  }
+//   const missingNeighbors = () => {
+//     if (guestsYes.length > 0) {
+//       let issues = []
+//       guestsYes.forEach((guestId) => {
+//         if (neighbors.length === 0 || !(neighbors.find(nbr => nbr.id === guestId))) {
+//           const missingNeighbor = guests.find(guest => guest.id === guestId)
+//           issues.push(`${makeInitials(missingNeighbor)} should sit next to ${thisGuestInitials}`)
+//         }
+//       })
+//       return issues
+//     } else {
+//       return []
+//     }
+//   }
 
-  const missingDescriptions = () => {
-    if (descriptionsYes.length > 0 && neighbors.length > 0) {
-      let issues = []
-      neighbors.forEach((neighbor) => {
-        descriptionsYes.forEach(desc => {
-          if (!(neighbor.traits.includes(desc))) {
-            issues.push(`${makeInitials(neighbor)} is not ${desc}`)
-          }
-        })
-      })
-      return issues
-    } else {
-      return []
-    }
-  }
+//   const wrongNeighbors = () => {
+//     if (guestsNo.length > 0 && neighbors.length > 0) {
+//       let issues = []
+//       guestsNo.forEach((guestId) => {
+//         const wrongNeighbor = neighbors.find(nbr => nbr.id === guestId)
+//         if (wrongNeighbor) {
+//           issues.push(`${makeInitials(wrongNeighbor)} should not sit next to ${thisGuestInitials}`)
+//         }
+//       })
+//       return issues
+//     } else {
+//       return []
+//     }
+//   }
 
-  const wrongDescriptions = () => {
-    if (descriptionsNo.length > 0 && neighbors.length > 0) {
-      let issues = []
-      neighbors.forEach((neighbor) => {
-        descriptionsNo.forEach(desc => {
-          if (neighbor.traits.includes(desc)) {
-            issues.push(`${makeInitials(neighbor)} is ${desc}`)
-          }
-        })
-      })
-      return issues
-    } else {
-      return []
-    }
-  }
+//   const missingDescriptions = () => {
+//     if (descriptionsYes.length > 0 && neighbors.length > 0) {
+//       let issues = []
+//       neighbors.forEach((neighbor) => {
+//         descriptionsYes.forEach(desc => {
+//           if (!(neighbor.traits.includes(desc))) {
+//             issues.push(`${makeInitials(neighbor)} is not ${desc}`)
+//           }
+//         })
+//       })
+//       return issues
+//     } else {
+//       return []
+//     }
+//   }
 
-  const newIssues = [
-    ...missingNeighbors(),
-    ...wrongNeighbors(),
-    ...missingDescriptions(),
-    ...wrongDescriptions(),
-  ]
+//   const wrongDescriptions = () => {
+//     if (descriptionsNo.length > 0 && neighbors.length > 0) {
+//       let issues = []
+//       neighbors.forEach((neighbor) => {
+//         descriptionsNo.forEach(desc => {
+//           if (neighbor.traits.includes(desc)) {
+//             issues.push(`${makeInitials(neighbor)} is ${desc}`)
+//           }
+//         })
+//       })
+//       return issues
+//     } else {
+//       return []
+//     }
+//   }
 
-  if (thisGuest.issues.length + newIssues.length > 0) {
-    return {
-      ...thisGuest,
-      issues: newIssues,
-    }
-  } else {
-    return thisGuest
-  }
-}
+//   const newIssues = [
+//     ...missingNeighbors(),
+//     ...wrongNeighbors(),
+//     ...missingDescriptions(),
+//     ...wrongDescriptions(),
+//   ]
+
+//   if (thisGuest.issues.length + newIssues.length > 0) {
+//     return {
+//       ...thisGuest,
+//       issues: newIssues,
+//     }
+//   } else {
+//     return thisGuest
+//   }
+// }
 
 // -----------------------------------------------
 
@@ -159,7 +161,9 @@ const events = (state = {
   selectedGuest: null,
 }, action) => {
 
-  let eventIdx, event, currentEvent, updatedEvent, guestIdx, guest, guests, selectedGuest, affectedGuests, chairs, chairIdx, chairId
+  // let eventIdx, event, currentEvent, updatedEvent, guestIdx, guest, guests, selectedGuest, affectedGuests, chairs, chairIdx, chairId
+
+  let eventIdx, event, currentEvent, updatedEvent, guestIdx, guest, selectedGuest, chairIdx, chairId
 
   const unJson = (string) => {
     if (string && string !== '') {
@@ -344,7 +348,7 @@ const events = (state = {
       //     selectedGuest: action.guest,
       //   }
 
-      case 'SELECT_GUEST':
+    case 'SELECT_GUEST':
       currentEvent = state.currentEvent
       guest = currentEvent.guests.find(guest => guest.id === action.guestId) || currentEvent.chairs.find(chair => chair.id === action.guestId)
       return {
@@ -442,7 +446,7 @@ const events = (state = {
         selectedGuest: null,
       }
 
-    case 'DELETE_GUEST':
+    case 'DESTROY_GUEST':
       eventIdx = state.savedEvents.findIndex(event => event.id === state.currentEvent.id)
       event = state.savedEvents[eventIdx]
       guestIdx = event.guests.findIndex(guest => guest.id === action.guest.id)
@@ -475,99 +479,100 @@ const events = (state = {
         selectedGuest: null,
       }
 
-    case 'UPDATE_NEIGHBORS':
-      eventIdx = state.savedEvents.findIndex(event => event.id === state.currentEvent.id)
-      event = state.savedEvents[eventIdx]
-      guests = event.guests
-      chairs = event.chairs
-      const guestBefore = action.guest
-      let guestAfter = guests.find(guest => guest.id === guestBefore.id)
+    // case 'UPDATE_NEIGHBORS':
+    //   eventIdx = state.savedEvents.findIndex(event => event.id === state.currentEvent.id)
+    //   event = state.savedEvents[eventIdx]
+    //   guests = event.guests
+    //   chairs = event.chairs
+    //   const guestBefore = action.guest
+    //   let guestAfter = guests.find(guest => guest.id === guestBefore.id)
 
-      const previousNeighbors = updatePreviousNeighbors(guestBefore, guests)
+    //   const previousNeighbors = updatePreviousNeighbors(guestBefore, guests)
 
-      if (guestAfter.seated) {
-        const newNeighborIds = getNewNeighborIds(chairs, guestAfter)
-        const newNeighbors = updateNewNeighbors(guests, newNeighborIds, chairs)
-        guestAfter = {
-          ...guestAfter,
-          neighbors: newNeighborIds,
-        }
-        affectedGuests = [
-          ...previousNeighbors,
-          guestAfter,
-          ...newNeighbors,
-        ]
-      } else {
-        affectedGuests = [...previousNeighbors]
-      }
+    //   if (guestAfter.seated) {
+    //     const newNeighborIds = getNewNeighborIds(chairs, guestAfter)
+    //     const newNeighbors = updateNewNeighbors(guests, newNeighborIds, chairs)
+    //     guestAfter = {
+    //       ...guestAfter,
+    //       neighbors: newNeighborIds,
+    //     }
+    //     affectedGuests = [
+    //       ...previousNeighbors,
+    //       guestAfter,
+    //       ...newNeighbors,
+    //     ]
+    //   } else {
+    //     affectedGuests = [...previousNeighbors]
+    //   }
 
-      if (affectedGuests.length > 0) {
-        const updatedGuests = affectedGuests.reduce((allGuests, updatedGuest) => {
-          guestIdx = guests.findIndex(guest => guest.id === updatedGuest.id)
-          return [
-            ...allGuests.slice(0, guestIdx),
-            updatedGuest,
-            ...allGuests.slice(guestIdx + 1),
-          ]
-        }, guests) 
-        updatedEvent = {
-          ...event,
-          guests: updatedGuests,
-          newlyAffectedGuests: affectedGuests,
-        }
-      } else {
-        updatedEvent = {
-          ...event,
-          newlyAffectedGuests: [],
-        }
-      }
+    //   if (affectedGuests.length > 0) {
+    //     const updatedGuests = affectedGuests.reduce((allGuests, updatedGuest) => {
+    //       guestIdx = guests.findIndex(guest => guest.id === updatedGuest.id)
+    //       return [
+    //         ...allGuests.slice(0, guestIdx),
+    //         updatedGuest,
+    //         ...allGuests.slice(guestIdx + 1),
+    //       ]
+    //     }, guests) 
+    //     updatedEvent = {
+    //       ...event,
+    //       guests: updatedGuests,
+    //       newlyAffectedGuests: affectedGuests,
+    //     }
+    //   } else {
+    //     updatedEvent = {
+    //       ...event,
+    //       newlyAffectedGuests: [],
+    //     }
+    //   }
 
-      return {
-        ...state,
-        savedEvents: [
-          ...state.savedEvents.slice(0, eventIdx),
-          updatedEvent,
-          ...state.savedEvents.slice(eventIdx + 1),
-        ],
-        currentEvent: updatedEvent,
-      }
+    //   return {
+    //     ...state,
+    //     savedEvents: [
+    //       ...state.savedEvents.slice(0, eventIdx),
+    //       updatedEvent,
+    //       ...state.savedEvents.slice(eventIdx + 1),
+    //     ],
+    //     currentEvent: updatedEvent,
+    //   }
 
     case 'CHECK_FOR_ISSUES':
-      eventIdx = state.savedEvents.findIndex(event => event.id === state.currentEvent.id)
-      event = state.savedEvents[eventIdx]
-      guests = event.guests
-      affectedGuests = event.newlyAffectedGuests
-      if (affectedGuests.length > 0) {
-        affectedGuests = affectedGuests.map(guest => checkForIssues(guest, guests))
-        const updatedGuests = affectedGuests.reduce((allGuests, updatedGuest) => {
-          guestIdx = guests.findIndex(guest => guest.id === updatedGuest.id)
-          return [
-            ...allGuests.slice(0, guestIdx),
-            updatedGuest,
-            ...allGuests.slice(guestIdx + 1),
-          ]
-        }, guests) 
-        updatedEvent = {
-          ...event,
-          guests: updatedGuests,
-          newlyAffectedGuests: [],
-        }
-      } else {
-        updatedEvent = {
-          ...event,
-          newlyAffectedGuests: [],
-        }
-      }
+      return state
+      // eventIdx = state.savedEvents.findIndex(event => event.id === state.currentEvent.id)
+      // event = state.savedEvents[eventIdx]
+      // guests = event.guests
+      // affectedGuests = event.newlyAffectedGuests
+      // if (affectedGuests.length > 0) {
+      //   affectedGuests = affectedGuests.map(guest => checkForIssues(guest, guests))
+      //   const updatedGuests = affectedGuests.reduce((allGuests, updatedGuest) => {
+      //     guestIdx = guests.findIndex(guest => guest.id === updatedGuest.id)
+      //     return [
+      //       ...allGuests.slice(0, guestIdx),
+      //       updatedGuest,
+      //       ...allGuests.slice(guestIdx + 1),
+      //     ]
+      //   }, guests) 
+      //   updatedEvent = {
+      //     ...event,
+      //     guests: updatedGuests,
+      //     newlyAffectedGuests: [],
+      //   }
+      // } else {
+      //   updatedEvent = {
+      //     ...event,
+      //     newlyAffectedGuests: [],
+      //   }
+      // }
 
-      return {
-        ...state,
-        savedEvents: [
-          ...state.savedEvents.slice(0, eventIdx),
-          updatedEvent,
-          ...state.savedEvents.slice(eventIdx + 1),
-        ],
-        currentEvent: updatedEvent,
-      }
+      // return {
+      //   ...state,
+      //   savedEvents: [
+      //     ...state.savedEvents.slice(0, eventIdx),
+      //     updatedEvent,
+      //     ...state.savedEvents.slice(eventIdx + 1),
+      //   ],
+      //   currentEvent: updatedEvent,
+      // }
 
     case 'CLEAR_EVENTS':
       return {
