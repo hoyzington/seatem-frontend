92a3b5ddae1d151f377badee830ba76ca02ae34c
Scott Hoisington <hoyzington@gmail.com>
2021-08-30 20:22:47 -0400

    improve code style with eslint xo-modified

--- src/store.js
+++ src/store.js
@@ -1,17 +1,17 @@
-import { createStore, applyMiddleware, compose, combineReducers } from 'redux'
-import currentUser from './reducers/currentUser'
-import events from './reducers/events'
-import errors from './reducers/errors'
-import thunk from 'redux-thunk'
+import { createStore, applyMiddleware, compose, combineReducers } from 'redux';
+import currentUser from './reducers/currentUser';
+import events from './reducers/events';
+import errors from './reducers/errors';
+import thunk from 'redux-thunk';
 
 const reducers = combineReducers({
-  currentUser,
-  events,
-  errors,
-})
+	currentUser,
+	events,
+	errors,
+});
 
-const composeEnhancer = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose
+const composeEnhancer = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
 
-const store = createStore(reducers, composeEnhancer(applyMiddleware(thunk)))
+const store = createStore(reducers, composeEnhancer(applyMiddleware(thunk)));
 
-export default store
+export default store;
