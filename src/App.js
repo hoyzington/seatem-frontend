8827348cab48238bcd9f97e6c8ac9ecd76bec563
Scott Hoisington <hoyzington@gmail.com>
2021-08-31 23:50:53 -0400

    improve code style with eslint xo-modified; add propTypes validations

--- src/App.js
+++ src/App.js
@@ -1,50 +1,56 @@
-import React from 'react'
+import React from 'react';
+import PropTypes from 'prop-types';
 import {
-  BrowserRouter as Router,
-  Route
-} from 'react-router-dom'
-import { connect } from 'react-redux'
-import NavBar from './components/NavBar'
-import FormArea from './containers/FormArea'
-import NonEventArea from './containers/NonEventArea'
-import EventArea from './containers/EventArea'
-import Footer from './components/Footer'
-import './styles/App.css'
+	BrowserRouter as Router,
+	Route,
+} from 'react-router-dom';
+import { connect } from 'react-redux';
+import NavBar from './components/NavBar';
+import FormArea from './containers/FormArea';
+import NonEventArea from './containers/NonEventArea';
+import EventArea from './containers/EventArea';
+import Footer from './components/Footer';
+import './styles/App.css';
 
 class App extends React.Component {
+	renderCurrentEvent() {
+		const { currentEvent } = this.props;
+		if (currentEvent) {
+			return (
+				<>
+					<NonEventArea table={currentEvent.table} />
+					<EventArea event={currentEvent} />
+				</>
+			);
+		}
+	}
 
-  renderCurrentEvent = () => {
-    const { event } = this.props
-    if (event) {
-      return (
-        <>
-          <NonEventArea table={event.table} />
-          <EventArea event={event} />
-        </>
-      )
-    }
-  }
-
-  render() {
-    const { user, event, errors } = this.props
-    return (
-      <Router>
-        <>
-          <NavBar user={user} event={event} />
-          <Route exact path='/' />
-          <FormArea user={user} event={event} errors={errors} />
-          {this.renderCurrentEvent()}
-          <Footer />
-        </>
-      </Router>
-    )
-  }
+	render() {
+		const { user, currentEvent, errors } = this.props;
+		return (
+			<Router>
+				<>
+					<NavBar user={user} event={currentEvent} />
+					<Route exact path="/" />
+					<FormArea user={user} currentEvent={currentEvent} errors={errors} />
+					{this.renderCurrentEvent()}
+					<Footer />
+				</>
+			</Router>
+		);
+	}
 }
 
-const mapStateToProps = (state) => ({
-  user: state.currentUser,
-  event: state.events.currentEvent,
-  errors: state.errors,
-})
+App.propTypes = {
+	user: PropTypes.string.isRequired,
+	currentEvent: PropTypes.objectOf(PropTypes.string).isRequired,
+	errors: PropTypes.arrayOf(PropTypes.string).isRequired,
+};
 
-export default connect(mapStateToProps)(App)
+const mapStateToProps = state => ({
+	user: state.currentUser,
+	currentEvent: state.events.currentEvent,
+	errors: state.errors,
+});
+
+export default connect(mapStateToProps)(App);
