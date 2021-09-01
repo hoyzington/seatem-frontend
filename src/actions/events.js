9df8cc72077080c0e3e7bb5ef3f3a7807404affe
Scott Hoisington <hoyzington@gmail.com>
2021-08-31 23:51:45 -0400

    improve code style with eslint xo-modified

--- src/actions/events.js
+++ src/actions/events.js
@@ -1,122 +1,99 @@
-import { logErrors, clearErrors } from './errors'
+import { logErrors, clearErrors } from './errors';
 
 // Synchronous Action Creators
 
-export const addEvent = (event) => {
-  return {
-    type: 'ADD_EVENT',
-    event,
-  }
-}
+export const addEvent = event => ({
+	type: 'ADD_EVENT',
+	event,
+});
 
-export const showEvent = (eventId) => {
-  return {
-    type: 'SHOW_EVENT',
-    eventId,
-  }
-}
+export const showEvent = eventId => ({
+	type: 'SHOW_EVENT',
+	eventId,
+});
 
-export const updateEvent = (id, changes) => {
-  return {
-    type: 'UPDATE_EVENT',
-    id,
-    changes,
-  }
-}
+export const updateEvent = (id, changes) => ({
+	type: 'UPDATE_EVENT',
+	id,
+	changes,
+});
 
-export const setSavedEvents = (events) => {
-  return {
-    type: 'SET_SAVED_EVENTS',
-    events,
-  }
-}
+export const setSavedEvents = events => ({
+	type: 'SET_SAVED_EVENTS',
+	events,
+});
 
-export const resetCurrentEvent = () => {
-  return {
-    type: 'RESET_CURRENT_EVENT',
-  }
-}
+export const resetCurrentEvent = () => ({
+	type: 'RESET_CURRENT_EVENT',
+});
 
-export const clearEvents = () => {
-  return {
-    type: 'CLEAR_EVENTS',
-  }
-}
+export const clearEvents = () => ({
+	type: 'CLEAR_EVENTS',
+});
 
-export const destroyEvent = (id) => {
-  return {
-    type: 'DESTROY_EVENT',
-    id,
-  }
-}
+export const destroyEvent = id => ({
+	type: 'DESTROY_EVENT',
+	id,
+});
 
 // Asynchronous Action Creators
 
-const baseUrl = 'http://localhost:3000/api/v1'
+const baseUrl = 'http://localhost:3000/api/v1';
 
-export const createEvent = (newEvent, userId) => {
-  return (dispatch) => {
-    dispatch(clearErrors())
-    return fetch(`${baseUrl}/users/${userId}/events`, {
-      method: 'POST',
-      headers: {
-        'Content-Type': 'application/json',
-        'Accept': 'application/json',
-      },
-      body: JSON.stringify(newEvent),
-    })
-      .then(res => res.json())
-      .then(createdEvent => {
-        if (createdEvent.errors) {
-          dispatch(logErrors(createdEvent.errors))
-        } else {
-          dispatch(addEvent(createdEvent))
-        }
-      })
-      .catch(console.log)
-  }
-}
+export const createEvent = (newEvent, userId) =>
+	dispatch => {
+		dispatch(clearErrors());
+		return fetch(`${baseUrl}/users/${userId}/events`, {
+			method: 'POST',
+			headers: {
+				'Content-Type': 'application/json',
+				'Accept': 'application/json',
+			},
+			body: JSON.stringify(newEvent),
+		})
+			.then(res => res.json())
+			.then(createdEvent => {
+				if (createdEvent.errors) {
+					dispatch(logErrors(createdEvent.errors));
+				} else {
+					dispatch(addEvent(createdEvent));
+				}
+			})
+			.catch(console.log);
+	};
 
-export const editEvent = (id, changes) => {
-  return (dispatch) => {
-    return fetch(`${baseUrl}/events/${id}`, {
-      method: 'PATCH',
-      headers: {
-        'Content-Type': 'application/json',
-        'Accept': 'application/json',
-      },
-      body: JSON.stringify(changes),
-    })
-      .then(res => res.json())
-      .then(json => {
-        if (json.errors) {
-          dispatch(logErrors(json.errors))
-        } else {
-          // alert(json.notice)
-        }
-      })
-      .catch(console.log)
-  }
-}
+export const editEvent = (id, changes) =>
+	dispatch => fetch(`${baseUrl}/events/${id}`, {
+		method: 'PATCH',
+		headers: {
+			'Content-Type': 'application/json',
+			'Accept': 'application/json',
+		},
+		body: JSON.stringify(changes),
+	})
+		.then(res => res.json())
+		.then(json => {
+			if (json.errors) {
+				dispatch(logErrors(json.errors));
+			}
+		})
+		.catch(console.log);
 
-export const deleteEvent = (id) => {
-  return (dispatch) => {
-    return fetch(`${baseUrl}/events/${id}`, {
-      method: 'DELETE',
-      headers: {
-        'Content-Type': 'application/json',
-        'Accept': 'application/json',
-      },
-    })
-      .then(res => res.json())
-      .then(json => {
-        if (json.errors) {
-          dispatch(logErrors(json.errors))
-        } else {
-          dispatch(destroyEvent(id))
-          // alert(json.notice)
-        }
-      })
-      .catch(console.log)
-  }
-}
+export const deleteEvent = id =>
+	dispatch => fetch(`${baseUrl}/events/${id}`, {
+		method: 'DELETE',
+		headers: {
+			'Content-Type': 'application/json',
+			'Accept': 'application/json',
+		},
+	})
+		.then(res => res.json())
+		.then(json => {
+			if (json.errors) {
+				dispatch(logErrors(json.errors));
+			} else {
+				dispatch(destroyEvent(id));
+			}
+		})
+		.catch(console.log);
+
