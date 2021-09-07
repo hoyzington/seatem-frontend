285da8244f6617f37bae53a966d2a3913193a019
Scott Hoisington <hoyzington@gmail.com>
2021-09-06 20:55:17 -0400

    remove unnecessary code

--- src/components/formsAndInfo/AccountInfo.js
+++ src/components/formsAndInfo/AccountInfo.js
@@ -4,14 +4,9 @@ import { NavLink } from 'react-router-dom';
 class AccountArea extends React.Component {
 	render() {
 		return (
-			<>
-				<div id="">
-
-				</div>
-				<div id="btn-area">
-					<NavLink className="btn form bottom" to="/" onClick={this.props.handleLogout} >LOG OUT</NavLink>
-				</div>
-			</>
+			<div id="btn-area">
+				<NavLink className="btn form bottom" to="/" onClick={this.props.handleLogout} >LOG OUT</NavLink>
+			</div>
 		);
 	}
 }
