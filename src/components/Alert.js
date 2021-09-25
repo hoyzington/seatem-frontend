c2e13f88ac2d74912e0842d42d5f2cf0f3881524
Scott Hoisington <hoyzington@gmail.com>
2021-09-24 22:18:40 -0400

    add propTypes to component

--- src/components/Alert.js
+++ src/components/Alert.js
@@ -1,5 +1,10 @@
 import React from 'react';
+import PropTypes from 'prop-types';
 
-const Alert = props => (<div>{props.message}</div>);
+const Alert = props => (<div id="alert">{props.message}</div>);
+
+Alert.propTypes = {
+	message: PropTypes.string,
+};
 
 export default Alert;
